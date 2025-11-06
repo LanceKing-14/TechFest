a0194e02a90c2f0e5247b8171ec87b9a89958fa3
King <12412532@benilde.edu.ph>
2025-11-06 12:19:47 +0800

    Participant registration

---
+++ TechFest.py
@@ -0,0 +1,8 @@
+print(" Welcome to SMIT TechFest ")
+print(" Event organized by Lance King of APPDAET BTCS2")
+
+number = (int(input("\nHow many participants will register: ")))
+
+if number <= 0:
+    print("\033[31mInvalid number of participants\033[0m")
+    exit()
