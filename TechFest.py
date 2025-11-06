45ce419f78df67491cf5260e24e35aa5567a3804
King <12412532@benilde.edu.ph>
2025-11-06 12:37:22 +0800

    Collect Participant Information

--- TechFest.py
+++ TechFest.py
@@ -6,3 +6,15 @@ number = (int(input("\nHow many participants will register: ")))
 if number <= 0:
     print("\033[31mInvalid number of participants\033[0m")
     exit()
+
+else:
+    participants = []
+
+for i in range(number):
+    name = input("\nWhat is your name? ")
+    track = input("What is your track? ")
+    participants.append({"name": name, "track":track})
+
+    print("\nRegistered participants:")
+    for i, p in enumerate(participants, 1):
+        print(f"{i}. {p['name']} - {p['track']}")
